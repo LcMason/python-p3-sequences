a9b6449935abdb5a3835f67e5d90232216113905
Lionel Mason <capitalcookhouse@gmail.com>
2024-11-17 15:52:43 -0500

    completes fibonacci lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,17 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        print ([])
+        return 
+        
+    fibonacci = [0, 1]
+
+    for i in range(2, length):
+        next_value = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+    
+    if length == 1:
+        fibonacci = [0]
+
+    print(fibonacci)
